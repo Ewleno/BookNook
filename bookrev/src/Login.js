bc649e528f4c24e9b6028b440118e669404e9676
jinan20056 <jinanabulawi05@gmail.com>
2025-12-02 17:03:54 +0000

    added register link

--- bookrev/src/Login.js
+++ bookrev/src/Login.js
@@ -1,5 +1,3 @@
-import "./Login.css";
-
 import React, { useState } from "react";
 import "./Login.css";
 
@@ -20,26 +18,31 @@ function Login() {
     <div className="login-container">
       <h2>Login Page</h2>
       <form onSubmit={handleLogin}>
-        <input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          className="login-input"
-        />
-        <br />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          className="login-input"
-        />
-        <br />
-        <button type="submit" className="login-button">
-          Sign In
-        </button>
-      </form>
+  <input
+    type="text"
+    placeholder="Username"
+    value={username}
+    onChange={(e) => setUsername(e.target.value)}
+    className="login-input"
+  />
+  <br />
+  <input
+    type="password"
+    placeholder="Password"
+    value={password}
+    onChange={(e) => setPassword(e.target.value)}
+    className="login-input"
+  />
+  <br />
+  <button type="submit" className="login-button">
+    Sign In
+  </button>
+
+  {/* <-- Put it right here, after the button */}
+  <p className="register-link">
+    Don't have an account? <a href="/register">Register</a>
+  </p>
+</form>
     </div>
   );
 }

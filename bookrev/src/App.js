0c45f84837c1aca388ac47b4b0ad84d6ba0f9943
Layan Ewies <148617440+Ewleno@users.noreply.github.com>
2025-12-03 13:47:58 +0000

    When pressing the sign up button you are redirected to the homepage

--- bookrev/src/App.js
+++ bookrev/src/App.js
@@ -14,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />   {/* <-- FIXED */}
         </Routes>
       </div>
       <Footer />

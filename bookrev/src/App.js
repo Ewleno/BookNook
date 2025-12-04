0d1f45aa9350285e19e1cd10a7ef2572cd38c126
jinan20056 <jinanabulawi05@gmail.com>
2025-12-04 12:37:58 +0000

    added the footer and navbar outseide the route so it can appear in all pages

--- bookrev/src/App.js
+++ bookrev/src/App.js
@@ -2,20 +2,22 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./navBar";
 import Footer from "./Footer";
-
 import Login from "./Login";
 import Register from "./Register";
 import Home from "./HomePage";
 
 function App() {
   return (
-    
+      <>
+   
+      <Navbar />
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
          <Route path="/home" element={<Home />} />
       </Routes>
-    
+     <Footer />
+    </>
   );
 }
 

c4fd233951cbd2bbff0bd5d2a9563ad97b23074d
batoolreyad115-bit <batoolreyad115@gmail.com>
2025-12-04 06:45:33 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="about">
+        //initial about
+      <header> <title> About us</title></header>
+      <body>
+      <h1> Who we are </h1>
+    <p>
+      <big><b>W</b></big>e are addicted to provide a 
+    </p>
+      </body>
+      </div>)
+}
+export default App;

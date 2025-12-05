954473bf1b2051ae2c7282786b73b7afab07c560
Layan Ewies <148617440+Ewleno@users.noreply.github.com>
2025-12-05 09:30:25 +0000

    Add redirects for Render

--- bookrev/src/Register.js
+++ bookrev/src/Register.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./Login.css"; // reuse the same styling
+import "./Login.css"; // reuse the same styling as the lgoin page 
 import { useNavigate, Link } from "react-router-dom";
 
 function Register() {
